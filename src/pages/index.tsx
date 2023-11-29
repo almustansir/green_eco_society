a33c4fc669299bc1a51c9a91608e3c7440abfef5
Samit_08 <almustansir465@gmail.com>
2023-11-29 08:31:18 +0600

    Nav bar added.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,48 +2,46 @@ export default function Home() {
   return (
     <div className="">
       <div
-        className="hero bg-scroll"
+        className="hero bg-scroll text-white"
         style={{
           backgroundImage: `url('/homeHeroBg.png')`,
           height: "972px",
         }}
       >
-        {/* 1. add hero image */}
-        {/* 2. align navbar */}
         {/* 3. add hero content */}
         {/* 4. design hero section */}
-        <nav className="">
-          <div className="">
+        <nav className="bg-neutral-950 bg-opacity-40">
+          <div className="w-[100%] flex justify-between p-3">
             <a href="#" className="">
               <img
                 src="nav/navIcon.png"
-                className="h-8 mr-3"
+                className="h-[60px]"
                 alt="Flowbite Logo"
               />
             </a>
             <div className="" id="navbar-sticky">
-              <ul className="s">
-                <li>
+              <ul className="flex justify-between">
+                <li className="p-4 hover:bg-slate-900 rounded-lg">
                   <a href="#" className="" aria-current="page">
                     Home
                   </a>
                 </li>
-                <li>
+                <li className="p-4 hover:bg-slate-900 rounded-lg">
                   <a href="#" className="">
                     About Us
                   </a>
                 </li>
-                <li>
+                <li className="p-4 hover:bg-slate-900 rounded-lg">
                   <a href="#" className="">
                     Gallery
                   </a>
                 </li>
-                <li>
+                <li className="p-4 hover:bg-slate-900 rounded-lg">
                   <a href="#" className="">
                     Executives
                   </a>
                 </li>
-                <li>
+                <li className="p-4 hover:bg-slate-900 rounded-lg">
                   <a href="#" className="">
                     Contact
                   </a>
@@ -56,96 +54,6 @@ export default function Home() {
         <h3 className="">Green Explore Society</h3>
         <button>Explore</button>
       </div>
-      {/* <div className="hero">
-        <nav className="bg-black-500 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="#" className="flex items-center">
-              <img
-                src="nav/navIcon.png"
-                className="h-8 mr-3"
-                alt="Flowbite Logo"
-              />
-            </a>
-            <div className="flex md:order-2">
-              <button
-                data-collapse-toggle="navbar-sticky"
-                type="button"
-                className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                aria-controls="navbar-sticky"
-                aria-expanded="false"
-              >
-                <span className="sr-only">Open main menu</span>
-                <svg
-                  className="w-5 h-5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 17 14"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M1 1h15M1 7h15M1 13h15"
-                  />
-                </svg>
-              </button>
-            </div>
-            <div
-              className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-              id="navbar-sticky"
-            >
-              <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-3 md:dark:text-blue-500"
-                    aria-current="page"
-                  >
-                    Home
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-3 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    About Us
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-3 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    Gallery
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-3 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    Executives
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-3 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    Contact
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-
-        <h3 className="mt-[75px]">Green Explore Society</h3>
-        <button>Explore</button>
-      </div> */}
     </div>
   );
 }
