cd166894ac631dd65bcddf44c3541a1c3283b5d2
Samit_08 <almustansir465@gmail.com>
2023-12-02 19:50:02 +0600

    Updated contacts links

--- src/Components/ContactsLinkComp.tsx
+++ src/Components/ContactsLinkComp.tsx
@@ -4,22 +4,28 @@ function ContactUsLinkComp() {
   return (
     <div className=" mt-20 flex flex-col space-y-12">
       {/* contactss link */}
-      <div className=" flex items-center">
-        <img
-          src="footer/fbIcon.png"
-          className="h-[35px] mr-8 hover:cursor-pointer"
-          alt="Flowbite Logo"
-        />
-        <a href="#">Facebook Page</a>
-      </div>
+      <div className="flex flex-col space-y-4">
+        <div className=" flex items-center">
+          <img
+            src="footer/fbIcon.png"
+            className="h-[35px] mr-8 hover:cursor-pointer"
+            alt="Flowbite Logo"
+          />
+          <a href="#" className=" hover:text-[#91C788]">
+            Facebook Page
+          </a>
+        </div>
 
-      <div className=" flex items-center">
-        <img
-          src="footer/fbIcon.png"
-          className="h-[35px] mr-8 hover:cursor-pointer"
-          alt="Flowbite Logo"
-        />
-        <a href="#">Facebook Page</a>
+        <div className=" flex items-center">
+          <img
+            src="footer/fbIcon.png"
+            className="h-[35px] mr-8 hover:cursor-pointer"
+            alt="Flowbite Logo"
+          />
+          <a href="#" className=" hover:text-[#91C788]">
+            Facebook Page
+          </a>
+        </div>
       </div>
 
       <div className=" flex items-center">
@@ -31,22 +37,24 @@ function ContactUsLinkComp() {
         <h3>example@gmail.com</h3>
       </div>
 
-      <div className=" flex items-center">
-        <img
-          src="footer/callIcon.png"
-          className="h-[35px] mr-8 hover:cursor-pointer"
-          alt="Flowbite Logo"
-        />
-        <h3>example@gmail.com</h3>
-      </div>
+      <div className="flex flex-col space-y-4">
+        <div className=" flex items-center">
+          <img
+            src="footer/callIcon.png"
+            className="h-[35px] mr-8 hover:cursor-pointer"
+            alt="Flowbite Logo"
+          />
+          <h3>+880 17687-65678</h3>
+        </div>
 
-      <div className=" flex items-center">
-        <img
-          src="footer/callIcon.png"
-          className="h-[35px] mr-8 hover:cursor-pointer"
-          alt="Flowbite Logo"
-        />
-        <h3>example@gmail.com</h3>
+        <div className=" flex items-center">
+          <img
+            src="footer/callIcon.png"
+            className="h-[35px] mr-8 hover:cursor-pointer"
+            alt="Flowbite Logo"
+          />
+          <h3>+880 17687-65678</h3>
+        </div>
       </div>
     </div>
   );
